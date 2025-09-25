92a45dfd75d548543c6f51a8b140a4b0d3e45a50
sehoon0529 <kim44415953@gmail.com>
2025-09-25 16:25:50 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+  const [count1, setCount1] = useState(0)
+  const [count2, setCount2] = useState(0)
+
+  return (
+    <div>
+      <Counter count={count1} onIncrement={() => setCount1(prev => prev + 1)} />
+      <Counter count={count2} onIncrement={() => setCount2(prev => prev + 1)} />
+    </div>
+  )
+}
+
+function Counter({ count, onIncrement }) {
+  return (
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={onIncrement}>
+        증가
+      </button>
+    </div>
+  )
+}
+
+export default App
